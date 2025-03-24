25d5e7abdde4e38187afbeaefed5eccaacc2812d
Bashorun Olusheyi <boboladvat@gmail.com>
2025-03-24 20:13:31 +0100

    first commit for this app

---
+++ src/main/java/com/bashenterprise/emsys/EmsysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bashenterprise.emsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsysBackendApplication.class, args);
+	}
+
+}
